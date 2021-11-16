d84ed225eb5ab797dec7ec11955ed1bb0c6cc663
AntonyRohithAkash <antonyrohtih.akash@team.neustar>
2021-11-17 01:11:08 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"terraform-provider-ultradns/ultradns"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: ultradns.Provider,
+	})
+}
